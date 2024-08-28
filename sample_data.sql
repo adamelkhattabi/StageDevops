774b5ad0b86d10ae463b561238bf027097ae5786
adamelkhattabi <adamelkt96@gmail.com>
2024-08-28 14:54:49 +0100

    commitlast

--- sample_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO player (name, position, age) VALUES
-('Lionel Messi', 'Forward', 34),
-('Cristiano Ronaldo', 'Forward', 36),
-('Kevin De Bruyne', 'Midfielder', 30),
-('Virgil van Dijk', 'Defender', 30),
-('Manuel Neuer', 'Goalkeeper', 35);
